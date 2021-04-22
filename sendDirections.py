3c3fd8bdaf8d990f07f2d423dd8abb68a91d990d
Michael Neises <neisesmichael@gmail.com>
2021-04-22 18:18:23 -0500

    update protocol

--- sendDirections.py
+++ sendDirections.py
@@ -4,18 +4,13 @@ import sys
 import base64
 import paramiko
 
+direction = sys.argv[1]
+
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.WarningPolicy())
-
 client.connect('192.168.1.14', username='pi', password='raspberry')
-
-direction = sys.argv[1]
-if(direction=="left"):
-    client.exec_command('sudo turnLeft.py')
-elif(direction=="center"):
-    client.exec_command('sudo turnCenter.py')
-elif(direction=="right"):
-    client.exec_command('sudo turnRight.py')
-
+stdin, stdout, stderr = client.exec_command('sudo takeDirection.py ' + direction)
+for line in stderr:
+    print('... ' + line.strip('\n'))
 client.close()
     
