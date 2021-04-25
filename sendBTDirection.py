2f10ef6f4ea0dcf33d2b3a6fe52333b33dbbc77f
Michael Neises <neisesmichael@gmail.com>
2021-04-25 00:55:21 -0500

    remove search to improve speed

--- sendBTDirection.py
+++ sendBTDirection.py
@@ -26,27 +26,17 @@ addr = None
 
 start = time.time()
 
-# search for the SampleServer service
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
-service_matches = bluetooth.find_service(uuid=uuid, address=addr)
 
 stamp1 = time.time()
 
-if len(service_matches) == 0:
-    print("Couldn't find the SampleServer service.")
-    sys.exit(0)
-
-first_match = service_matches[0]
-port = first_match["port"]
-name = first_match["name"]
-host = first_match["host"]
-
-print("Connecting to \"{}\" on {}".format(name, host))
 stamp2 = time.time()
+
+print(host)
+print(port)
 
 # Create the client socket
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-sock.connect((host, port))
+sock.connect(("B8:27:EB:EC:85:F0", "1"))
 
 stamp3 = time.time()
 sock.send(direction)
