104f88587e0ef0f3c331c544be7cf82419126e75
Michael Neises <neisesmichael@gmail.com>
2021-04-25 00:48:20 -0500

    add timing measures

--- sendBTDirection.py
+++ sendBTDirection.py
@@ -10,6 +10,8 @@ $Id: rfcomm-client.py 424 2006-08-24 03:35:54Z albert $
 import sys
 
 import bluetooth
+import time
+
 
 direction = sys.argv[1]
 
@@ -22,9 +24,13 @@ addr = None
 #    addr = sys.argv[1]
 #    print("Searching for SampleServer on {}...".format(addr))
 
+start = time.time()
+
 # search for the SampleServer service
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 service_matches = bluetooth.find_service(uuid=uuid, address=addr)
+
+stamp1 = time.time()
 
 if len(service_matches) == 0:
     print("Couldn't find the SampleServer service.")
@@ -36,11 +42,22 @@ name = first_match["name"]
 host = first_match["host"]
 
 print("Connecting to \"{}\" on {}".format(name, host))
+stamp2 = time.time()
 
 # Create the client socket
 sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
 sock.connect((host, port))
 
+stamp3 = time.time()
 sock.send("left")
 
+stamp4 = time.time()
+
 sock.close()
+
+
+print("timing results:")
+print(stamp1 - start)
+print(stamp2 - stamp1)
+print(stamp3 - stamp2)
+print(stamp4 - stamp3)
